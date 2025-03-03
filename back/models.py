66225fc7371ee238408c85b786cbd78fecfb82ac
RaazDk1992 <dhakal1992raaz@gmail.com>
2025-03-03 17:29:05 +0545

    profle added.

--- back/models.py
+++ back/models.py
@@ -1,4 +1,23 @@
 from django.db import models
+from django.dispatch import receiver
+from django.db.models.signals import post_save
+from django.contrib.auth.models import User
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User,on_delete=models.CASCADE)
+    firstName = models.CharField(max_length=100)
+    lastName  = models.CharField(max_length=100)
+    is_active = models.BooleanField(default=False)
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+@receiver(post_save, sender=User)
+def save_profile(sender,instance, **kwargs):
+        instance.profile.save()
+
 
 
 class ContentTypes(models.Model):
