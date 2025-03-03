7fe13b8db0d84b07e61aa86662ae81959dcc8a43
RaazDk1992 <dhakal1992raaz@gmail.com>
2025-03-03 22:27:42 +0545

    proxy modified.

--- back/proxy.py
+++ back/proxy.py
@@ -1,5 +1,6 @@
 import requests
 from django.http import JsonResponse, HttpResponse
+from django.shortcuts import render
 def proxy_view(request,path):
     BACKEND_URL = "http://127.0.0.1:8000/back/" 
     target_url = f"{BACKEND_URL}{path}"
@@ -14,3 +15,13 @@ def proxy_view(request,path):
         return JsonResponse(response.json(), safe=False)
     except requests.exceptions.RequestException as e:
         return JsonResponse({"error": str(e)}, status=500)
+
+def  proxy_view_with_render(request,path):
+    BACKEND_URL = "http://127.0.0.1:8000/back/"
+    headerx = {"X-CSRFToken": request.META.get("CSRF_COOKIE")}  
+
+    target_url = f"{BACKEND_URL}{path}"
+    if request.method == "GET":
+        response = requests.get(target_url,params=request.GET,headers=headerx)
+        if response.status_code ==200:
+            return render(request,'back/login.html')
