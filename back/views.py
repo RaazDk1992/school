b8bc50e52ec4dfb3d27f882065fa9bdd6fc9a20e
RaazDk1992 <dhakal1992raaz@gmail.com>
2025-03-05 11:18:58 +0545

    updates on ui

--- back/views.py
+++ back/views.py
@@ -1,8 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.http import HttpResponse,JsonResponse
 from django.contrib.auth  import authenticate,login,logout
 from back.forms.menu import MenuForm,SubMenuFormSet
-from back.models import Menu,SubMenu
 from back.forms.newArticle import ArticleForm
 
 # Create your views here.
@@ -24,9 +23,9 @@ def userLogin(request):
         user =  authenticate(request,username=userName,password=password)
         if user is not None:
             login(request,user)
-            return JsonResponse({"message":"Success"})
+            return JsonResponse({"message":"Success"},status=200)
         else:
-            return JsonResponse({"status":"failed","message":"Bad Credentials"})
+            return JsonResponse({"status":"failed","message":"Bad Credentials"}, status=401)
     return render(request,'back/login.html')
 
 def loadDashboard(request):
@@ -37,22 +36,21 @@ def loadDashboard(request):
             return render(request,"back/login.html")
 def addMenu(request):
     if not request.user.is_authenticated:
-        return redirect('login')  # Redirect to login if not authenticated
+        return redirect('login')  
 
     menu_form = MenuForm(request.POST or None)
     submenu_formset = SubMenuFormSet(request.POST or None)
 
     if request.method == 'POST':
         if menu_form.is_valid() and submenu_formset.is_valid():
-            menu = menu_form.save()  
-            print(menu)
+            menu = menu_form.save()
             submenu_instances = submenu_formset.save(commit=False) 
             print(submenu_instances)
             for submenu in submenu_instances:
                 submenu.menuRef = menu
                 submenu.save()  
 
-            submenu_formset.save_m2m()  # Ensure many-to-many fields are saved if any
+            #submenu_formset.save_m2m()  # Ensure many-to-many fields are saved if any
 
             #return redirect('success_url')  # Redirect after saving
 
