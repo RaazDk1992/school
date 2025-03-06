e7cbacac6f0d6e9f5d36133c55427a124042cb31
RaazDk1992 <dhakal1992raaz@gmail.com>
2025-03-06 21:56:05 +0545

    time added iin gallery

--- back/models.py
+++ back/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.dispatch import receiver
 from django.db.models.signals import post_save
 from django.contrib.auth.models import User
+from django.utils import timezone
 from django_ckeditor_5.fields import CKEditor5Field
 
 
@@ -29,8 +30,10 @@ class Notices(models.Model):
         return self.noticeTitle
 
 class Gallery(models.Model):
+
     galleryName = models.CharField(max_length=200)
     is_active = models.BooleanField(default=True)
+    creation_date = models.DateField(default=timezone.now)
 
 
     def __str__(self):
