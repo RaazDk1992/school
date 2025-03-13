345d38396dfef4e3dee3191db59d5d2a7fb8faec
RaazDk1992 <dhakal1992raaz@gmail.com>
2025-03-13 12:41:07 +0545

    navbar issues fixwd

--- back/forms/menu.py
+++ back/forms/menu.py
@@ -1,6 +1,5 @@
-# forms.py
 from django import forms
-from back.models import Menu, SubMenu,ContentType
+from back.models import Menu, SubMenu, ContentType
 from django.forms import modelformset_factory
 
 class MenuForm(forms.ModelForm):
@@ -9,17 +8,32 @@ class MenuForm(forms.ModelForm):
         widget=forms.Select(attrs={'class': 'form-control'}),
         empty_label="Select a Content Type"
     )
+
+    # Ensure viewRef is optional and has a default
+    viewRef = forms.CharField(
+        required=False, 
+        initial="front.views.dynamicView",
+        widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'View Reference (Optional)'})
+    )
+
     class Meta:
         model = Menu
         fields = '__all__'
-       
         widgets = {
             'menuItem': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Enter menu item name'}),
             'menuPath': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Enter menu path'}),
             'is_active': forms.CheckboxInput(attrs={'class': 'form-check-input'}),
             'is_expandable': forms.CheckboxInput(attrs={'class': 'form-check-input'}),
-            
         }
+
+    def save(self, commit=True):
+        instance = super().save(commit=False)
+        if not instance.viewRef:  # Apply default if empty
+            instance.viewRef = "front.views.dynamicView"
+        if commit:
+            instance.save()
+        return instance
+
 
 class SubMenuForm(forms.ModelForm):
     class Meta:
@@ -28,5 +42,6 @@ class SubMenuForm(forms.ModelForm):
         widgets = {
             'submenu': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Submenu Name'})
         }
+
 
 SubMenuFormSet = modelformset_factory(SubMenu, form=SubMenuForm, extra=1)
--- front/urls.py
+++ front/urls.py
@@ -1,7 +1,8 @@
-from django.urls import path
+from django.urls import path,include
 from front.customurls import dynamicPaths
 from . import views
 urlpatterns =[
+    
     path('',views.index,name="index"),
     path('notices/',views.loadNotices,name="notices"),
     path('gallery/',views.showGalleries,name="gallery"),
